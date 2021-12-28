2ce7eb2de63059c47f21e889d614f5209c381b67
Lluis Franco <hello@lluisfranco.com>
2021-12-28 15:57:14 +0100

    Added support for EXIF tags on images

--- ImageExtensions.cs
+++ ImageExtensions.cs
@@ -8,32 +8,32 @@ using System.Threading.Tasks;
 
 namespace FacebookFixDates
 {
-    public enum MetaProperty
-    {
-        Title = 40091,
-        Comment = 40092,
-        Author = 40093,
-        Keywords = 40094,
-        Subject = 40095,
-        Copyright = 33432,
-        Software = 11,
-        DateTime = 36867
-    }
+    //public enum MetaProperty
+    //{
+    //    Title = 40091,
+    //    Comment = 40092,
+    //    Author = 40093,
+    //    Keywords = 40094,
+    //    Subject = 40095,
+    //    Copyright = 33432,
+    //    Software = 11,
+    //    DateTime = 36867
+    //}
 
-    internal static class ImageExtensions
-    {
-        public static string GetMetaValue(this Bitmap sourceBitmap, MetaProperty property)
-        {
-            PropertyItem[] propItems = sourceBitmap.PropertyItems;
-            var prop = propItems.FirstOrDefault(p => p.Id == (int)property);
-            if (prop != null)
-            {
-                return Encoding.UTF8.GetString(prop.Value);
-            }
-            else
-            {
-                return null;
-            }
-        }
-    }
+    //internal static class ImageExtensions
+    //{
+    //    public static string GetMetaValue(this Bitmap sourceBitmap, MetaProperty property)
+    //    {
+    //        PropertyItem[] propItems = sourceBitmap.PropertyItems;
+    //        var prop = propItems.FirstOrDefault(p => p.Id == (int)property);
+    //        if (prop != null)
+    //        {
+    //            return Encoding.UTF8.GetString(prop.Value);
+    //        }
+    //        else
+    //        {
+    //            return null;
+    //        }
+    //    }
+    //}
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using HtmlAgilityPack;
+﻿using ExifLibrary;
+using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -11,23 +12,12 @@ namespace FacebookFixDates
         public static FacebookParser FacebookParser { get; set; } = new FacebookParser();
         static void Main(string[] args)
         {
-            Console.WriteLine("****************************************************************************************");
-            Console.WriteLine("** FACEBOOK BACKUP - FIX PHOTOS DATES                                                 **");
-            Console.WriteLine("** When you backup your Facebook profile a ZIP file is generated with all your info.  **");
-            Console.WriteLine("** Once unzipped, there is a folder named 'photos_and_videos which contains all your  **");
-            Console.WriteLine("** albums, profile and timeline photos, organized in folders.                         **");
-            Console.WriteLine("** These folders contains your photos but some metadata has been removed (like date)  **");
-            Console.WriteLine("** This script reads the content of this profile, reading from the HTML pages,        **");
-            Console.WriteLine("** and fix the photo metadata. Then copy the fixed photos to a new folder.            **");
-            Console.WriteLine("****************************************************************************************");
-            Console.WriteLine();
-            Console.WriteLine("Enter your Facebook backup base path ('C:\\FB' (Win) or '/home/<user>/FB' (Linux or Mac)");
-
             const string FB_PHOTOS_FOLDER_NAME = "photos_and_videos";
             const string FB_PHOTOS_INDEX_PAGE_NAME = "your_photos.html";
-            
-            const string EXPORT_ALBUMS_FOLDER_NAME = "_ExportedPhotos";
+            const string EXPORT_ALBUMS_FOLDER_NAME = "_Export";
             const bool EXPORT_USE_ALBUM_FOLDER_NAME = true;
+
+            PrintHeader();
 
             var facebook_base_path = Console.ReadLine();
 
@@ -45,7 +35,7 @@ namespace FacebookFixDates
                 var facebook_photos_path = Path.GetFullPath(
                     Path.Combine(FacebookParser.BaseFolderPath, FB_PHOTOS_FOLDER_NAME));
                 var facebook_photos_folder = new DirectoryInfo(facebook_photos_path);
-                FacebookParser.PhotosFolderPath = facebook_photos_path; 
+                FacebookParser.PhotosFolderPath = facebook_photos_path;
                 if (facebook_photos_folder.Exists)
                 {
                     var facebook_photos_index_page_path = Path.GetFullPath(
@@ -100,7 +90,13 @@ namespace FacebookFixDates
                                     photo.Date = photoDate;
                                     album.Photos.Add(photo);
 
-                                    
+                                    //var file = ImageFile.FromFile(photoURL);
+
+                                    //var isoTag = file.Properties.Get<ExifUShort>(ExifTag.ISOSpeedRatings);
+                                    //var dateTag = file.Properties.Get<ExifDateTime>(ExifTag.DateTimeOriginal);
+                                    //var newdate = dateTag.Value.AddDays(1);
+                                    //file.Properties.Set(ExifTag.DateTimeOriginal, newdate);
+                                    //file.Save(photoURL);
                                 }
                             }
                             FacebookParser.Albums.Add(album);
@@ -120,6 +116,21 @@ namespace FacebookFixDates
             {
                 PrintItemDontExistsMessage(FacebookParser.BaseFolderPath);
             }
+        }
+
+        private static void PrintHeader()
+        {
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("** FACEBOOK BACKUP - FIX PHOTOS DATES                                                 **");
+            Console.WriteLine("** When you backup your Facebook profile a ZIP file is generated with all your info.  **");
+            Console.WriteLine("** Once unzipped, there is a folder named 'photos_and_videos which contains all your  **");
+            Console.WriteLine("** albums, profile and timeline photos, organized in folders.                         **");
+            Console.WriteLine("** These folders contains your photos but some metadata has been removed (like date)  **");
+            Console.WriteLine("** This script reads the content of this profile, reading from the HTML pages,        **");
+            Console.WriteLine("** and fix the photo metadata. Then copy the fixed photos to a new folder.            **");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine();
+            Console.WriteLine("Enter your Facebook backup base path ('C:\\FB' (Win) or '/home/<user>/FB' (Linux or Mac)");
         }
 
         private static void PrintItemDontExistsMessage(string item_path)
