e8ff904d8ad656a774b98a6fafe51355b94aaa60
Lluis Franco <hello@lluisfranco.com>
2022-01-12 15:57:29 +0100

    GetDateMetadata on ImageExtensions

--- Extensions/ImageExtensions.cs
+++ Extensions/ImageExtensions.cs
@@ -1,22 +1,23 @@
 ﻿using ExifLibrary;
 using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FacebookFixDates
 {
     public static class ImageExtensions
     {
+        public static DateTime? GetDateMetadata(string photoURL)
+        {
+            var file = ImageFile.FromFile(photoURL);
+            var date = file.Properties.Get<ExifDateTime>(ExifTag.DateTimeOriginal);
+            if (date?.Value != null)
+                return date;
+            else
+                return null;
+        }
+
         public static void SaveDateMetadata(string photoURL, DateTime date)
         {
             var file = ImageFile.FromFile(photoURL);
-            //var dateTag = file.Properties.Get<ExifDateTime>(ExifTag.DateTimeOriginal);
-            //var photoExifDate = dateTag?.Value ?? DateTime.Today;
-            //var newdate = photoExifDate.AddDays(1);
             file.Properties.Set(ExifTag.DateTimeOriginal, date);
             file.Save(photoURL);
         }
